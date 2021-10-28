06c91a7f9afd15a3858a5202285dc1715894b72a
ahmadraza330 <ra9557408@gmail.com>
2021-10-27 22:28:47 -0700

    first

---
+++ src/context/cart_context.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  TOGGLE_CART_ITEM_AMOUNT,
+  CLEAR_CART,
+  COUNT_CART_TOTALS,
+} from "../actions";
+
+const getLocalStorage = () => {
+  const cart = localStorage.getItem("cartItem");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cartItem"));
+  } else {
+    return [];
+  }
+};
+
+const initialState = {
+  cart: getLocalStorage(),
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 553,
+};
+
+const CartContext = React.createContext();
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, amount, color, product } });
+  };
+
+  const removeItem = (id) => {};
+  const toggleAmount = (id, value) => {};
+  const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cartItem", JSON.stringify(state.cart));
+  }, [state.cart]);
+  return (
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+// make sure use
+export const useCartContext = () => {
+  return useContext(CartContext);
+};
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import AuthWrapper from "./AuthWrapper";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+};
